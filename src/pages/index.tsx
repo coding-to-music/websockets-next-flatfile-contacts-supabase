18b760a5ec203c13a7a3016e67b3a07e722d14b1
coding-to-music <connors.tom@gmail.com>
2023-09-12 16:04:33 -0500

    Build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,145 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Spreadsheet from "react-spreadsheet";
+import { useDebouncedCallback } from "use-debounce";
+import axios from "axios";
 
-const inter = Inter({ subsets: ['latin'] })
+const debouncer = useDebouncedCallback((newData: any, diff) => {
+  setData((oldData) => {
+    // update the server with our new data
+    updateServer(diff);
+    return newData;
+  });
+}, 500);
+
+const updateServer = useCallback(
+  (serverData?: { value: string; col: number; row: number }) => {
+    if (!serverData) {
+      return;
+    }
+    console.log(serverData);
+    return axios.post("/api/update-record", serverData);
+  },
+  []
+);
+
+const setNewData = (newData: { value: string }[][], ignoreDiff?: boolean) => {
+  // This function will tell us what actually changed in the data (the column / row)
+  const diff = findDiff(data, newData);
+
+  // Only if there was not real change, or we didn't ask to ignore changes, trigger the debouncer.
+  if (diff || ignoreDiff) {
+    return debouncer(newData, diff);
+  }
+};
+
+const findDiff = useCallback(
+  (oldData: { value: string }[][], newData: { value: string }[][]) => {
+    for (let i = 0; i < oldData.length; i++) {
+      for (let y = 0; y < oldData[i].length; y++) {
+        if (oldData[i][y] !== newData[i][y]) {
+          return {
+            oldValue: oldData[i][y].value,
+            value: newData[i][y].value,
+            row: i,
+            col: y,
+          };
+        }
+      }
+    }
+  },
+  []
+);
+
+// Add a new column
+const addCol = useCallback(() => {
+  setNewData(
+    data.length === 0
+      ? [[{ value: "" }]]
+      : data.map((p: any) => [...p, { value: "" }]),
+    true
+  );
+}, [data]);
+
+// Add a new row
+const addRow = useCallback(() => {
+  setNewData(
+    [...data, data?.[0]?.map(() => ({ value: "" })) || [{ value: "" }]],
+    true
+  );
+}, [data]);
+
+// Remove a column by index
+const removeCol = useCallback(
+  (index: number) => (event: any) => {
+    setNewData(
+      data.map((current) => {
+        return [...current.slice(0, index), ...current.slice((index || 0) + 1)];
+      }),
+      true
+    );
+    event.stopPropagation();
+  },
+  [data]
+);
+
+// Remove a row by index
+const removeRow = useCallback(
+  (index: number) => (event: any) => {
+    setNewData(
+      [...data.slice(0, index), ...data.slice((index || 0) + 1)],
+      true
+    );
+    event.stopPropagation();
+  },
+  [data]
+);
 
 export default function Home() {
+  const [data, setData] = useState<{ value: string }[][]>([]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex justify-center items-stretch">
+      <div className="flex flex-col">
+        <Spreadsheet
+          columnLabels={data?.[0]?.map((d, index) => (
+            <div
+              key={index}
+              className="flex justify-center items-center space-x-2"
+            >
+              <div>{String.fromCharCode(64 + index + 1)}</div>
+              <div className="text-xs text-red-500" onClick={removeCol(index)}>
+                X
+              </div>
+            </div>
+          ))}
+          rowLabels={data?.map((d, index) => (
+            <div
+              key={index}
+              className="flex justify-center items-center space-x-2"
+            >
+              <div>{index + 1}</div>
+              <div className="text-xs text-red-500" onClick={removeRow(index)}>
+                X
+              </div>
+            </div>
+          ))}
+          darkMode={true}
+          data={data}
+          className="w-full"
+          onChange={setNewData}
+        />
+        <div
+          onClick={addRow}
+          className="bg-[#060606] border border-[#313131] border-t-0 mb-[6px] flex justify-center py-1 cursor-pointer"
+        >
+          +
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div
+        onClick={addCol}
+        className="bg-[#060606] border border-[#313131] border-l-0 mb-[6px] flex items-center px-3 cursor-pointer"
+      >
+        +
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
